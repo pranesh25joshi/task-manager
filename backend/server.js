9a23429762b79d600edb7c6a1ee3fedcf2b56af8
Pranesh <pranesh25joshi@gmail.com>
2025-12-31 00:06:02 +0530

    configure cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
